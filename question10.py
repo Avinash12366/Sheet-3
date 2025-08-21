dc53b8ef845336a19d1334bc8e19a98b4510f6e4
Avinash Chaudhary <avinash.chaudhary_bca24@gla.ac.in>
2025-08-21 10:26:24 +0530

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,10 +1,11 @@
 num1 = int(input("enter a number\n:"))
 num2 = int(input("enter a number\n:"))
-so = 1
+ans = 1
 for i in range(num2):
-    so = so*num1
-    print(so.end=" ")
+    ans = ans*num1
+    print(ans.end=" ")
 
 
   
+
 
